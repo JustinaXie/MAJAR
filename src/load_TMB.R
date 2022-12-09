27de856f2f15d327db9a1c6e430c32256f64bea4
JustinaXie <32473819+JustinaXie@users.noreply.github.com>
2022-12-09 17:05:13 -0500

    Update load_TMB.R

--- src/load_TMB.R
+++ src/load_TMB.R
@@ -1,2 +1,4 @@
 TMB::compile("src/TMB/tau2_rho_f_v2.cpp",CPPFLAGS="-Wno-ignored-attributes")
 TMB::compile("src/TMB/tau2_rho_f_v1.cpp",CPPFLAGS="-Wno-ignored-attributes")
+dyn.load(dynlib("tau2_rho_f_v2"))
+dyn.load(dynlib("tau2_rho_f_v1"))
